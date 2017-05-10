52548d3739eb64d7750eec28d7ced0951ba141b6
Devin Shoemaker <devinshoe@gmail.com>
2017-05-09 19:02:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Spring Web DB Example
+Demo project for Spring Boot with web MVC, Spring Security and JPA
+
+Run Project
+-
+`mvn spring-boot:run`
+
+Run Tests
+-
+`mvn test`
---
+++ src/main/java/com/devinshoemaker/spring/web/db/example/SpringWebDbExampleApplication.java
@@ -0,0 +1,12 @@
+package com.devinshoemaker.spring.web.db.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebDbExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebDbExampleApplication.class, args);
+    }
+}
